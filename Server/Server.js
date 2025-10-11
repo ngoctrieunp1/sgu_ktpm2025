ac2ea605b7adeb3ad69d7ebb286ba1737ace6167
ngoctrieunp1 <ngoctrieunp11@gmail.com>
2025-10-11 20:31:21 +0700

    Up Docker

--- Server/Server.js
+++ Server/Server.js
@@ -1,22 +1,27 @@
-const express=require("express")
-const cors=require("cors")
-const app=express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-require("./config/db")
+require("./config/db");
+
 app.use(cors({
-    origin:"http://localhost:3000",
-    methods:"*"
-}))
-app.use(express.json())
-const myroutes=require("./Routers/Routes")
-const userRoutes=require("./Routers/UserRoutes")
-const CartRoutes=require("./Routers/CartRoutes")
-const orderRoutes=require("./Routers/OrderRoutes")
-app.use("/",myroutes)
-app.use("/",userRoutes)
-app.use("/",CartRoutes)
-app.use("/",orderRoutes)
-app.listen(4000,(err)=>{
-    if (err) process.exit(1);
-    console.log("server is running");
-})
+  origin: "http://localhost:3000",
+  methods: "*"
+}));
+app.use(express.json());
+
+const myroutes = require("./Routers/Routes");
+const userRoutes = require("./Routers/UserRoutes");
+const CartRoutes = require("./Routers/CartRoutes");
+const orderRoutes = require("./Routers/OrderRoutes");
+
+app.use("/", myroutes);
+app.use("/", userRoutes);
+app.use("/", CartRoutes);
+app.use("/", orderRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, (err) => {
+  if (err) process.exit(1);
+  console.log("server is running");
+});
--- Server/config/db.js
+++ Server/config/db.js
@@ -1,7 +1,28 @@
-require("dotenv").config()
-const mongoose=require("mongoose")
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
-    console.log("db connected");
-}).catch((err)=>{
-    console.log(err);
-})
+// Server/config/db.js
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const uri =
+  process.env.MONGODB_URI ||
+  process.env.MONGODB_URL ||
+  'mongodb://host.docker.internal:27017/foodapp';
+
+mongoose.connection.on('connected', () => {
+  console.log('mongoose connected to', uri);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('mongoose disconnected');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('db connection error:', err.message);
+});
+
+(async () => {
+  try {
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 10000,
+    });
+  } catch (err) {
+    console.error('initial connect failed:', err.message);
+  }
+})();
