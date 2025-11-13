2a43ea528a3b4932722e05e8f226f7ae31bb3dc4
ngoctrieunp1 <ngoctrieunp11@gmail.com>
2025-11-13 13:03:24 +0700

    render frontend

--- Client/my-app/src/axios.js
+++ Client/my-app/src/axios.js
@@ -1,13 +1,32 @@
-import axios from "axios"
-import {getToken} from "./Pages/Signup/LocalStorage/LocalDb"
+// import axios from "axios"
+// import {getToken} from "./Pages/Signup/LocalStorage/LocalDb"
 
-export const api=axios.create({
-    baseURL:"http://localhost:4000"
-})
+// export const api=axios.create({
+//     baseURL:"http://localhost:4000"
+// })
 
 
-api.interceptors.request.use(config=>{
-    const token=getToken() || null
-    config.headers.Authorization=`Bearer ${token}`
-    return config
-})
+// api.interceptors.request.use(config=>{
+//     const token=getToken() || null
+//     config.headers.Authorization=`Bearer ${token}`
+//     return config
+// })
+
+
+import axios from "axios";
+import { getToken } from "./Pages/Signup/LocalStorage/LocalDb";
+
+const baseURL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:4000";
+
+export const api = axios.create({
+  baseURL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken() || null;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
